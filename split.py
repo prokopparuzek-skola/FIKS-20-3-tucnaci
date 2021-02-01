3002062e076cdb31f84da9722a61ae1f4d6f41ba
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2021-02-01 18:27:01 +0100

    split images to smaller ones

---
+++ split.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from glob import glob
+from PIL import Image
+
+pictures = glob("input/*")
+pictures.sort()
+i = 0
+
+for p in pictures:
+    img = Image.open(p)
+    for y in range(0, 480, 120):
+        for x in range(0, 480, 120):
+            cropImg = img.crop((x, y, x+120, y+120))
+            cropImg.save(f"model/{i:04}.pbm")
+            i += 1
---
+++ unpack.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv input.txt input.tar.gz
+tar xfa input.tar.gz
